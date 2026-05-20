ca12e2588aef752df415f6e5904ee20582558d53
abashidzeluka <abashidze2003@gmail.com>
2026-05-21 00:46:55 +0400

    Initial commit

---
+++ Assignment1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrey1 = [1, 2, 3];
+            int[] arrey2 = [4, 5, 6];
+
+            int[] resultArrey = new int[arrey1.Length + arrey2.Length];
+
+            for (int i = 0; i < arrey1.Length; i++)
+            {
+                resultArrey[i] = arrey1[i];
+            }
+
+            for (int i = 0; i < arrey2.Length; i++)
+            {
+                resultArrey[arrey1.Length + i] = arrey2[i];
+            }
+
+            foreach (int i in resultArrey)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}

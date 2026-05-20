cadf9b08e74d7965230939b6f9385dd47b3850ae
abashidzeluka <abashidze2003@gmail.com>
2026-05-21 01:10:10 +0400

    Initial commit

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            int[] array
+            int[] array = { 3, 5, -4, 8, 11, 1, -1, 6 };
+
+            int targetSum = 7;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] + array[j] == targetSum)
+                    {
+                        Console.WriteLine($"[{array[i]}, {array[j]}]");
+                    }
+                }
+            }
         }
     }
 }
